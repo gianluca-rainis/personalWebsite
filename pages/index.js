8ef83b1ce5089c6889045824d94dc6c059e47379
gianluca-rainis <gianlucarainis@gmail.com>
2026-04-30 08:00:00 +0200

    Fixed the wrong email address.

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function HomePage() {
                 <h1>Gianluca Rainis</h1>
                 <p>This site is still work in progress.</p>
                 <p>In the meanwhile you can visit my GitHub and my LinkedIn.</p>
-                <p>If you have any questions, feel free to contact me via Email at <a href='mailto:gianluca.rainis@gianlucarainis.com'>gianluca.rainis@gianlucarainis.com</a>.</p>
+                <p>If you have any questions, feel free to contact me via Email at <a href='mailto:gianlucarainis@gianlucarainis.com'>gianlucarainis@gianlucarainis.com</a>.</p>
                 <div className='social-links'>
-                    <a href='mailto:gianluca.rainis@gianlucarainis.com' target='_blank' rel='noopener noreferrer'>
+                    <a href='mailto:gianlucarainis@gianlucarainis.com' target='_blank' rel='noopener noreferrer'>
                         <img src='https://skillicons.dev/icons?i=gmail' alt='Gmail' />
                     </a>
                     <a href='https://github.com/gianluca-rainis' target='_blank' rel='noopener noreferrer'>
